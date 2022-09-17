9d851436f3a686b3c608b97aa821fcb3e2fdacf9
Eric Samuelsson <gossen02@gmail.com>
2022-09-17 08:39:52 +0100

    Allows documents to be created and loaded into the editor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
-import "trix";
-import "trix/dist/trix.css";
-
+import 'trix';
+import 'trix/dist/trix.css';
 import { TrixEditor } from "react-trix";
+import docsModel from './models/docsModel';
 
 function App() {
-  const [value, setValue] = useState("Skriv något");
-  const printContent = () => {
-    console.log(value);
-  }
-  return (
-    <div className="App">
-      <button className="saveButton" onClick={printContent}>Spara</button>
-        <TrixEditor
-          value = {value}
-          onChange = {setValue}
-        />
-    </div>
-  );
+    const [content, setContent] = useState("");
+    const [docs, setDocs] = useState([]);
+    const [currentDoc, setCurrentDoc] = useState({});
+    const [title, setTitle] = useState("");
+
+    useEffect(() => {
+        (async () => {
+            const allDocs = await docsModel.getAllDocs();
+            setDocs(allDocs);
+        })();
+    }, [currentDoc]);
+
+    function fetchDoc() {
+        const selectedDocId = document.getElementById("selectDoc").value;
+        const selectedDoc = docs[selectedDocId];
+
+        setCurrentDoc(selectedDoc);
+
+        document.getElementById("saveBtn").style.display = "none";
+        document.getElementById("updateBtn").style.display = "block";
+        document.getElementById("nameLabel").style.display = "none";
+
+        if (selectedDoc) {
+            setEditorContent(selectedDoc);
+        }
+    };
+
+    function setEditorContent(selectedDoc) {
+        const element = document.querySelector("trix-editor");
+        element.value = "";
+        element.editor.insertHTML(selectedDoc.content);
+    }
+
+    async function saveDoc() {
+        const newDoc = {
+            title: title,
+            content: content
+        }
+
+        await docsModel.saveDoc(newDoc);
+        window.location.reload()
+    }
+
+    async function updateDoc() {
+        currentDoc.content = content;
+        await docsModel.updateDoc(currentDoc);
+    }
+
+    return (
+        <div className="app">
+            <button onClick={() => window.location.reload()}>Skapa nytt</button>
+
+            <select
+                id="selectDoc"
+                onChange={fetchDoc}
+            >
+                <option value="-99" key="0">Välj ett dokument</option>
+                {docs.map((doc, index) => <option value={index} key={index}>{doc.title}</option>)}
+            </select>
+
+            <button style={{display: "none"}} id="updateBtn" onClick={() => updateDoc()}>Uppdatera</button>
+
+            <form id="nameLabel">
+                <label>Skriv in namn på filen:
+                    <input
+                    type="text"
+                    value={title}
+                    onChange={(event) => setTitle(event.target.value)}
+                    />
+                </label>
+            </form>
+
+            <button id="saveBtn" onClick={() => saveDoc()}>Spara</button>
+
+            <TrixEditor
+                onChange={setContent}
+            />
+        </div>
+    )
 }
 
 export default App;
