689e7170f76622fe67c843afb873e5b33c95e9ae
Eric Samuelsson <gossen02@gmail.com>
2022-09-23 13:26:12 +0100

    Add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import { render, screen } from '@testing-library/react';
-import React from 'react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders a button to create new document', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const createBtn = screen.getByText("Skapa nytt");
+  expect(createBtn).toBeInTheDocument();
 });
+
+test('renders a specific text', () => {
+  render(<App />);
+  const selectText = screen.getByText("Välj ett dokument");
+  expect(selectText).toBeInTheDocument();
+});
+
+test('renders a specific label text', () => {
+  render(<App />);
+  const labelText = screen.getByLabelText("Skriv in namn på filen:");
+  expect(labelText).toBeInTheDocument();
+});
+
